e61b34bf4ffc2f0ca4b70aaf4b3275db525051e9
The Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-16 00:01:51 -0500

    Added Design addon

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -12,3 +12,11 @@ PrimaryButton.args = {
     label: 'Send now'
 }
 
+PrimaryButton.parameters = {
+    design: {
+        type: "figma",
+    //   url: "https://www.figma.com/file/NVYR5zWuWM4Y0yRdwOSOIn/zero-height-test?node-id=37%3A48",
+        // url: "https://www.figma.com/proto/NVYR5zWuWM4Y0yRdwOSOIn/zero-height-test?page-id=0%3A1&node-id=93%3A121&viewport=2400%2C-3345%2C0.94&scaling=min-zoom&starting-point-node-id=93%3A121&show-proto-sidebar=1",
+        url: "https://www.figma.com/proto/NVYR5zWuWM4Y0yRdwOSOIn/zero-height-test?page-id=0%3A1&node-id=93%3A124&viewport=1424%2C-2257%2C0.66&scaling=min-zoom&starting-point-node-id=93%3A124&show-proto-sidebar=1"
+    },
+  };
