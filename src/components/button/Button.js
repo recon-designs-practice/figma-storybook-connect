c425202710c6b7d6fc3f0b1bd101a5ead44f52d6
The Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-16 00:34:09 -0500

    Commit

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -11,10 +11,10 @@ const ButtonComponent = styled.button`
   font-size: 16px;
   line-height: 20px;
   text-transform: uppercase;
-  color: #F2F1EF;
-  background: #0E8AD0;
-  border: 2px solid #2D2E31;
-  box-shadow: 6px 6px 0px rgba(45, 46, 49, 0.14);
+  color: ${props => props.color};
+  background: ${props => props.background};
+  border: ${props => props.border};
+  box-shadow: ${props => props.boxShadow};
   border-radius: 2px;
 
   &:hover {
@@ -32,18 +32,65 @@ const ButtonComponent = styled.button`
 `
 
 export default function Button(props) {
+  const { label, buttonType } = props
+  const { 
+    background,
+    color,
+    border,
+    boxShadow
+  } = getButtonTypeStyles(buttonType)
+
+  function getButtonTypeStyles(type) {
+    const lowerCaseType = type.toLowerCase()
+
+    switch (lowerCaseType) {
+      case 'primary':
+      default:
+        return {
+          color: '#F2F1EF',
+          background: '#0E8AD0',
+          border: '2px solid #2D2E31',
+          boxShadow: '6px 6px 0px rgba(45, 46, 49, 0.14)'
+        }
+      case 'secondary':
+        return {
+          color: '#2D2E31',
+          background: '#F2F1EF',
+          border: '2px solid #2D2E31',
+          boxShadow: '6px 6px 0px rgba(45, 46, 49, 0.14)'
+        }
+      case 'text':
+        return {
+          color: '#0E8AD0',
+          background: 'none',
+          border: 'none',
+          boxShadow: 'none'
+        }
+    }
+  }
+
   return (
-    <ButtonComponent>{props.label}</ButtonComponent>
+    <ButtonComponent
+      color={color}
+      background={background}
+      border={border}
+      boxShadow={boxShadow}
+    >{label}</ButtonComponent>
   )
 }
 
 Button.defaultProps = {
-  label: 'Button'
+  label: 'Button',
+  buttonType: 'primary'
 }
 
 Button.propTypes = {
   /** 
-   * Text to be printed on the button
+   * Text to be printed on the Button
    */
-  label: PropTypes.string
+  label: PropTypes.string,
+  /**
+   * Sets the themed styling for the Button
+   */
+  buttonType: PropTypes.oneOf(['primary', 'secondary', 'text'])
 }
