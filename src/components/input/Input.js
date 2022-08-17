c83800234887d9ae0b6c5f3ca734d9306959d04c
The Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-17 00:10:08 -0500

    Added basic styling to Input component

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,17 +1,56 @@
 import React from 'react'
+import styled from 'styled-components'
 import HelperText from './HelperText'
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const InputWrapper = styled.div`
+  margin-bottom: 8px;
+  display: flex;
+  flex-direction: column;
+`
+
+const HelperTextWrapper = styled.div`
+  /* border: 1px solid blue; */
+`
+
+const Label = styled.label`
+  margin-bottom: 4px;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 16px;
+  color: #45464A;
+`
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  padding: 8px;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 20px;
+  color: #45464A;
+  background: #FFFFFF;
+  border: 2px solid #B1B2B4;
+  border-radius: 2px;
+`
 
 export default function Input(props) {
   return (
-    <div>
-        <div>
-          <label style={{ display: 'flex' }}>Label</label>
-          <input placeholder={props.placeholder} />
-        </div>
-        <div>
+    <InputContainer>
+        <InputWrapper>
+          <Label>Label</Label>
+          <StyledInput placeholder={props.placeholder} />
+        </InputWrapper>
+        <HelperTextWrapper>
           <HelperText>Here is some text to help.</HelperText>
-        </div>
-    </div>
+        </HelperTextWrapper>
+    </InputContainer>
   )
-  
 }
