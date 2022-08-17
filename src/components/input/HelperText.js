36e7761764ae7cdd0d63f153e4eaa07c6185113b
The Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-16 23:06:28 -0500

    Started Input component

---
+++ src/components/input/HelperText.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function HelperText({ children }) {
+  return <p>{children}</p>
+}
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import HelperText from './HelperText'
 
 export default function Input(props) {
-  return <input placeholder={props.placeholder} />
+  return (
+    <div>
+        <div>
+          <label style={{ display: 'flex' }}>Label</label>
+          <input placeholder={props.placeholder} />
+        </div>
+        <div>
+          <HelperText>Here is some text to help.</HelperText>
+        </div>
+    </div>
+  )
+  
 }
